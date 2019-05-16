61f9c20d118e5bdc5238c094e40c8159248ed8f7
Randy <rlthomasccv@gmail.com>
2019-05-16 10:59:27 -0700

    Must fix app table issues

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 1992;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
